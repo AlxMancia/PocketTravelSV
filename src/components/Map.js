4b3574a3e494e2bb4c87d8a85934049ff84facef
pertuzdev <antonio.pertuz01@gmail.com>
2021-11-01 15:19:38 -0600

    Update prettier format

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,83 +1,78 @@
-import React, { useEffect, useRef } from 'react'
+import React, {useEffect, useRef} from 'react';
 import MapView, {Marker} from 'react-native-maps';
-import { useLocation } from '../hook/useLocation';
-import { LoadingScreen } from '../screens/LoadingScreen';
-import { FabResetButton } from './FabResetButton';
+import {useLocation} from '../hook/useLocation';
+import {LoadingScreen} from '../screens/LoadingScreen';
+import {FabResetButton} from './FabResetButton';
 
 export const Map = () => {
+  const {
+    hasLocation,
+    initialPosition,
+    getCurrentLocation,
+    followUserLocation,
+    userLocation,
+    stopFollowingUserPosition,
+  } = useLocation();
 
-    const { hasLocation,
-            initialPosition,
-            getCurrentLocation,
-            followUserLocation,
-            userLocation,
-            stopFollowingUserPosition } = useLocation();
+  const mapViewRef = useRef();
+  const following = useRef(true);
 
-    const mapViewRef = useRef();
-    const following = useRef(true);
+  // useEffect(() => {
+  //     followUserLocation();
+  //     return()=>{
+  //         stopFollowingUserPosition();
+  //     }
+  // }, [])
 
+  useEffect(() => {
+    if (!followUserLocation) return;
 
-    // useEffect(() => {
-    //     followUserLocation();
-    //     return()=>{
-    //         stopFollowingUserPosition();
-    //     }
-    // }, [])
+    const {latitude, longitude} = userLocation;
+    mapViewRef.current?.animateCamera({
+      center: {
+        latitude: latitude,
+        longitude: longitude,
+      },
+    });
+  }, [userLocation]);
 
-    useEffect(() => {
-        if(!followUserLocation) return;
+  const centerPosition = async () => {
+    const location = await getCurrentLocation();
+    following.current = true;
+    mapViewRef.current.animateCamera({
+      center: {
+        latitude: location.latitude,
+        longitude: location.longitude,
+      },
+    });
+  };
 
-       const {latitude,longitude} = userLocation;
-       mapViewRef.current?.animateCamera({
-            center:{
-                latitude: latitude,
-                longitude:longitude
-            }
-       })
-    }, [userLocation])
+  if (!hasLocation) return <LoadingScreen />;
 
-
-    const centerPosition = async () =>{
-        const location = await getCurrentLocation();
-        following.current = true;
-        mapViewRef.current.animateCamera({
-            center:{
-                latitude: location.latitude,
-                longitude:location.longitude
-            }
-        })
-    }
-
-    if(!hasLocation) return <LoadingScreen/>;
-
-    // console.log(initialPosition);
-    return (
-        <>
-            <MapView
-                ref={ (el)=> mapViewRef.current = el }
-                style={{flex:1}}
-                showsUserLocation={true}
-                initialRegion={{
-                latitude: initialPosition.latitude,
-                longitude: initialPosition.longitude,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421,
-                }}
-                onTouchStart={()=>following.current = false}
-            >
-                <Marker
-                    coordinate={{
-                        latitude:13.684757,
-                        longitude:-89.301191,
-                    }}
-                    title="Marcador"
-                    description="Prueba de marcador"
-                />
-            </MapView>
-            <FabResetButton
-                onPress={centerPosition}
-            />
-        </>
-    )
-}
-
+  // console.log(initialPosition);
+  return (
+    <>
+      <MapView
+        ref={el => (mapViewRef.current = el)}
+        style={{flex: 1}}
+        showsUserLocation={true}
+        initialRegion={{
+          latitude: initialPosition.latitude,
+          longitude: initialPosition.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        onTouchStart={() => (following.current = false)}>
+        <Marker
+          coordinate={{
+            latitude: 13.684757,
+            longitude: -89.301191,
+          }}
+          title="Marcador"
+          description="Prueba de marcador"
+        />
+      </MapView>
+      <FabResetButton onPress={centerPosition} />
+    </>
+  );
+};
