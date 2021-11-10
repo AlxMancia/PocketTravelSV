f6aa11939343050500d6bae6a5b13fb77a9d02a9
danielorozco14 <00200617@uca.edu.sv>
2021-11-09 18:38:53 -0600

    Trazado de rutas agregado

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,6 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import MapView, {Marker} from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
 import {useLocation} from '../hook/useLocation';
 import {LoadingScreen} from '../screens/LoadingScreen';
 import {FabResetButton} from './FabResetButton';
@@ -27,6 +28,26 @@ export const Map = () => {
   //     }
   // }, [])
 
+  //Dummy data for routes.
+  const [coordinates] = useState([
+    {
+      latitude: 13.6917741,
+      longitude: -89.2900771,
+    },
+    {
+      latitude: 13.7017741,
+      longitude: -89.2800771,
+    },
+  ]);
+
+  //MARKER 1. POSICIONES PARA HACER TRAZADO DE RUTA
+  const [endLong, setEndLong] = useState();
+  const [endLat, setEndLat] = useState();
+
+  //MARKER 2. POSICIONES PARA HACER TRAZADO DE RUTA
+  const [endLong2, setEndLong2] = useState();
+  const [endLat2, setEndLat2] = useState();
+
   useEffect(() => {
     if (!followUserLocation) return;
 
@@ -51,8 +72,8 @@ export const Map = () => {
   };
 
   if (!hasLocation) return <LoadingScreen />;
-   console.log(initialPosition.latitude)
-   console.log(initialPosition.longitude)
+  console.log(initialPosition.latitude);
+  console.log(initialPosition.longitude);
 
   return (
     <>
@@ -62,7 +83,7 @@ export const Map = () => {
           Geolocation.getCurrentPosition(
             info => console.log(info.coords),
             err => console.log(err),
-            {timeout:100000,enableHighAccuracy: true},
+            {timeout: 100000, enableHighAccuracy: true},
           )
         }
       />
@@ -77,15 +98,47 @@ export const Map = () => {
           longitudeDelta: 0.0421,
         }}
         onTouchStart={() => (following.current = false)}>
-        <Marker
+        <MapViewDirections
+          origin={coordinates[0]}
+          destination={coordinates[1]}
+          apikey={'AIzaSyDRv1WE_WRGcptxSyPD8o9wPNFLai7wLS0'} // insert your API Key here
+          strokeWidth={4}
+          strokeColor="#111111"
+        />
+       <Marker
           coordinate={{
-            latitude: 13.686959,
-            longitude: -89.101190,
+            latitude: 13.684757,
+            longitude: -89.2900386,
+          }}
+          draggable={true}
+          onDragStart={e => {
+            console.log('onDrag', e.nativeEvent.coordinate);
+          }}
+          onDragEnd={e => {
+            setEndLat2(e.nativeEvent.coordinate.latitude);
+            setEndLong2(e.nativeEvent.coordinate.longitude);
           }}
           title="Marcador"
           description="Prueba de marcador"
         />
-        </MapView>
+
+        <Marker
+          coordinate={{
+            latitude: 13.684757,
+            longitude: -89.301191,
+          }}
+          draggable={true}
+          onDragStart={e => {
+            console.log('onDrag', e.nativeEvent.coordinate);
+          }}
+          onDragEnd={e => {
+            setEndLat(e.nativeEvent.coordinate.latitude);
+            setEndLong(e.nativeEvent.coordinate.longitude);
+          }}
+          title="Marcador"
+          description="Prueba de marcador"
+        />
+      </MapView>
       <FabResetButton onPress={centerPosition} />
     </>
   );
